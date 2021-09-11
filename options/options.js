37ac22d5a40dd71a6446fad9089e6b83fe388ac4
Crowdbotics <team@crowdbotics.com>
2021-09-11 18:25:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "avipay_30472Identifier",
+  url: "https://avipay_30472Identifier.botics.co",
+  api: "https://avipay_30472Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
